8bd65b71b1c6f1c4bc532c97e5cdf38539686dcd
IOSOM01 <om@unolo.com>
2025-11-01 23:36:21 +0530

    fix .env issue

--- db.js
+++ db.js
@@ -3,20 +3,14 @@ require("dotenv").config();
 //const monogUrl = "mongodb://localhost:27017/hotels";
 const monogUrl = process.env.MONGODB_URL;
 
-mongoose.connect(monogUrl);
+mongoose
+  .connect(monogUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Connection error:", err));
 
 const db = mongoose.connection;
-
-db.on("connected", () => {
-  console.log("connected to mongodb");
-});
-
-db.on("disconnected", () => {
-  console.log("disconnected from mongodb");
-});
-
-db.on("error", () => {
-  console.log("mongodb connection error");
-});
 
 module.exports = db;
