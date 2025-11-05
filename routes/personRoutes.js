053bb8db1f0005281f35d2b400c89d6ca9db49f6
IOSOM01 <om@unolo.com>
2025-11-05 06:16:31 +0530

    implement jwt token

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -2,20 +2,64 @@ const express = require("express");
 const router = express.Router();
 
 const Person = require("./../models/Person");
+const { jsonWebAuthToken, generateToken } = require("../middleware/jwt");
 
-router.post("/", async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
     const data = req.body;
 
     const newPerson = new Person(data);
     let response = await newPerson.save();
-    res.status(200).json(response);
+
+    const token = generateToken(response.username);
+    console.log("token", token);
+    res.status(200).json({ response: response, token: token });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-router.get("/", async (req, res) => {
+router.post("/login", async (req, res) => {
+  try {
+    const user = await Person.findOne({ username: req.body.username });
+
+    console.log(user);
+
+    // if (!user || !(await user.comparePassword(user.password))) {
+    //   return res.status(404).json({ error: "Invalid username or password" });
+    // }
+
+    if (!user) {
+      return res.status(404).json({ error: "Invalid username or password" });
+    }
+
+    const payload = {
+      id: user.id,
+      username: user.username,
+    };
+
+    const token = generateToken(payload);
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get("/personProfile", jsonWebAuthToken, async (req, res) => {
+  try {
+    const userData = req.user;
+
+    const userID = userData.id;
+
+    const user = await Person.findById(userID);
+
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/", jsonWebAuthToken, async (req, res) => {
   try {
     const personRecord = await Person.find();
     res.status(200).json(personRecord);
